143f938c75805c8bc472c8739ca18b2cc9520b0c
ap-pradeep <anipindipradeep@gmail.com>
2024-06-11 10:01:00 +0530

    First

---
+++ firstprog.cpp
@@ -0,0 +1,10 @@
+
+#include<iostream>
+using name space std;
+int main()
+{
+    a = 5;
+    b = 6;
+    int c;
+    c = a + b;
+}
---
+++ variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using name space std;
+int main()
+{
+    a = 5;
+    b = 6;
+    int c;
+    c = a + b;
+}
